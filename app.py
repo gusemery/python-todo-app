adacb2a97429b4e79e0afb2f9a70fc8303baa52b
Gus Emery <nfemery@gmail.com>
2020-03-25 12:01:27 -0500

    Updated Rookout

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ import requests
 from datetime import datetime
 from random import randint
 from todos_store import Store
-from rook import auto_start
+
 
 app = flask.Flask(__name__, static_url_path='/static')
+
 
 # unsafeRandId generates a random string composed from english upper case letters and digits
 # it's called unsafe because it doesn't use a crypto random generator
